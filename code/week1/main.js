92761db89741ea1668dd2c26525b5473dcd7f309
Annelie <a.berner@ciid.dk>
2020-03-06 15:23:59 +0100

    code

---
+++ code/week1/main.js
@@ -0,0 +1,26 @@
+var a = 5;
+var getViz = document.getElementById("viz");
+getViz.innerHTML += " " +a;
+
+//variables
+//arrays
+//accessing arrays
+//[]
+//objects
+//{}
+//functions
+//for loops
+//operators
+//! = == < > <= >= / 
+//conditionals
+//comparisons
+//logic if(){} 
+//comments
+//scope: global / function level
+//svg
+//svg shapes
+//drawing order
+
+
+
+
