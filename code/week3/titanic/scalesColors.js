7b5bba28b08c924b7d51612c9e93934b255a3c80
Annelie <a.berner@ciid.dk>
2020-03-26 15:03:02 +0100

    titanic

---
+++ code/week3/titanic/scalesColors.js
@@ -0,0 +1,29 @@
+	const colScale = d3.scaleLinear()
+		.domain([0, dataset.length])
+ 		.range(["green", "lightgreen", "purple"])
+    	.interpolate(d3.interpolateRgb.gamma(2.2))
+		
+	const xScale = d3.scaleLinear()
+		.domain([0, dataset.length]) 
+		.range([margin, screenWidth - margin]) 
+	
+	const domainY = function (d) {
+		return d.y;
+	}
+	const yScale = d3.scaleLinear()
+		.domain(d3.extent(dataset, domainY))
+		.range([screenHeight-margin, margin])
+
+	const circles = canvas.selectAll("circ")		
+		.data(dataset)
+		.enter().append("circle")
+		.attr("cx", function (d, i) {		
+			return xScale(i);
+		})
+		.attr("cy", function (d) {
+			return yScale(domainY(d));
+		})
+		.attr("r", 10)
+		.attr("fill", function (d, i) {	
+			return colScale(i);
+		})
---
+++ code/week3/titanic/titanic.js
@@ -0,0 +1,59 @@
+
+async function drawData() {
+/*step 1: get the data and see one piece of it*/	
+	const dataset = await d3.csv("train.csv");
+	const accessOnePiece = dataset[0];
+	console.log(accessOnePiece);
+
+/*side-steps to a network: 
+1. find all last names that occur more than once
+
+2. set up special process of data where we say that:
+if a data item shares a last name with another data item
+then
+associate them as linked
+
+3. set up a "force network" where d3
+draws nodes (circles) and links (arcs) for each family
+and alone nodes for non-family
+
+*/	
+
+/*step 2: define dimensions of canvas*/
+	const screenWidth = window.innerWidth * 0.9;
+	const screenHeight = window.innerHeight * 0.9;
+	const margin = 10;
+
+/*step 3: make the canvas*/
+	const myCanvas = d3.select("#wrapper") //grab this element with the idea of wrapper
+		.append("svg") //add an SVG canvas
+		.attr("width", screenWidth) //of this width
+		.attr("height", screenHeight); //and this height
+
+/*step 4: draw shapes with random messy positions just to see the data*/	
+
+/*step 5: try to set up a scale*/	
+	const domainAge = function (d) {
+		return parseInt(d.age);
+	}
+	const yScale = d3.scaleLinear() //linear range want quantitative information
+		.domain(d3.extent(dataset, domainAge)) //find min and max of what's coming in from my dataseet
+		.range([screenHeight-10, 10]); //what i want to come out on the screen
+	console.log(d3.extent(dataset, domainAge))
+/*step 6: try to redraw the shapes now with at least a scale
+for example... 
+*/
+/*step 7: use scale to draw shapes on the canvas :) */
+	const dataCirc = myCanvas.selectAll("dc")
+		.data(dataset)
+		.enter()
+		.append("circle")
+		.attr("cx", 50)
+		.attr("cy", function(d){
+			return yScale(parseInt(d.age));
+		})
+		.attr("r",10)
+		.attr("fill","pink");
+
+}
+drawData();
