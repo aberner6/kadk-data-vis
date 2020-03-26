0ae973adf5245a51752deef9893be944943b44f0
Annelie <a.berner@ciid.dk>
2020-03-26 17:22:09 +0100

    milas

--- code/week3/photos/photos.js
+++ code/week3/photos/photos.js
@@ -5,55 +5,69 @@ async function drawData() {
 	const accessOnePiece = dataset[0];
 	console.log(accessOnePiece);
 
-/*side-steps to a network: 
-1. find all last names that occur more than once
-
-2. set up special process of data where we say that:
-if a data item shares a last name with another data item
-then
-associate them as linked
-
-3. set up a "force network" where d3
-draws nodes (circles) and links (arcs) for each family
-and alone nodes for non-family
-
-*/	
+/*step _: set up date parser
+"11/10/2015"
+https://github.com/d3/d3-time-format#locale_format
+https://bl.ocks.org/zanarmstrong/ca0adb7e426c12c06a95
+http://learnjsdata.com/time.html
+*/
+	const dateParser = d3.timeParse("%m/%d/%Y");
+	const xAccessor = function(d){
+		return dateParser(d.date);
+	} 
 
 /*step 2: define dimensions of canvas*/
-	// const screenWidth = window.innerWidth * 0.9;
-	// const screenHeight = window.innerHeight * 0.9;
-	// const margin = 10;
+	const screenWidth = window.innerWidth * 0.9;
+	const screenHeight = window.innerHeight * 0.9;
+	const margin = 10;
 
 /*step 3: make the canvas*/
-	// const myCanvas = d3.select("#wrapper") //grab this element with the idea of wrapper
-	// 	.append("svg") //add an SVG canvas
-	// 	.attr("width", screenWidth) //of this width
-	// 	.attr("height", screenHeight); //and this height
+	const myCanvas = d3.select("#wrapper") //grab this element with the idea of wrapper
+		.append("svg") //add an SVG canvas
+		.attr("width", screenWidth) //of this width
+		.attr("height", screenHeight); //and this height
+
+/*step _: set up xscale according to date*/
+	const xScale = d3.scaleTime()
+		.domain(d3.extent(dataset, xAccessor))
+		.range([margin, screenWidth-margin])
+
+    console.log(d3.extent(dataset, xAccessor))
+/*step _: set up bar scale according to likes*/
+	const domainLikes = function (d) {
+		return parseInt(d.likes);
+	}
+	const yScale = d3.scaleLinear() //linear range want quantitative information
+		.domain(d3.extent(dataset, domainLikes)) //find min and max of what's coming in from my dataseet
+		.range([screenHeight-margin, margin]); //what i want to come out on the screen
+	console.log(d3.extent(dataset, domainLikes))
+/*step _: set up fill scale according to color data*/
+
+
 
 /*step 4: draw shapes with random messy positions just to see the data*/	
 
-/*step 5: try to set up a scale*/	
-	// const domainAge = function (d) {
-	// 	return parseInt(d.age);
-	// }
-	// const yScale = d3.scaleLinear() //linear range want quantitative information
-	// 	.domain(d3.extent(dataset, domainAge)) //find min and max of what's coming in from my dataseet
-	// 	.range([screenHeight-10, 10]); //what i want to come out on the screen
-	// console.log(d3.extent(dataset, domainAge))
+
 /*step 6: try to redraw the shapes now with at least a scale
 for example... 
 */
-/*step 7: use scale to draw shapes on the canvas :) */
-	// const dataCirc = myCanvas.selectAll("dc")
-	// 	.data(dataset)
-	// 	.enter()
-	// 	.append("circle")
-	// 	.attr("cx", 50)
-	// 	.attr("cy", function(d){
-	// 		return yScale(parseInt(d.age));
-	// 	})
-	// 	.attr("r",10)
-	// 	.attr("fill","pink");
-
+/*step 7: use scale to draw shapes on the canvas :) 
+check the week2/friday/bars.js example for more on this
+*/
+	const dataBar = myCanvas.selectAll("db")
+		.data(dataset)
+		.enter()
+		.append("rect")
+		.attr("x", function(d){
+			return xScale(xAccessor(d)); //the x accessor parses the date for us
+		})
+		.attr("y", function(d){
+			return yScale(d.likes);
+		})
+		.attr("width", 10)
+		.attr("height", function(d){
+			return screenHeight-yScale(d.likes);
+		})
+		.attr("fill","pink");
 }
 drawData();
