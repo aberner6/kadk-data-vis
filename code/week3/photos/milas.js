1a3e9daa7659f8367a9e30dfccda3b42d292202a
Annelie <a.berner@ciid.dk>
2020-03-27 15:02:14 +0100

    milas

--- code/week3/photos/milas.js
+++ code/week3/photos/milas.js
@@ -1,7 +1,7 @@
 
 async function drawData() {
 /*step 1: get the data and see one piece of it*/	
-	const dataset = await d3.csv("milas.csv");
+	const dataset = await d3.csv("milas_sm.csv");
 	const accessOnePiece = dataset[0];
 	console.log(accessOnePiece);
 
@@ -19,7 +19,7 @@ http://learnjsdata.com/time.html
 /*step 2: define dimensions of canvas*/
 	const screenWidth = window.innerWidth * 0.9;
 	const screenHeight = window.innerHeight * 0.9;
-	const margin = 10;
+	const margin = 40;
 
 /*step 3: make the canvas*/
 	const myCanvas = d3.select("#wrapper") //grab this element with the idea of wrapper
@@ -39,8 +39,13 @@ http://learnjsdata.com/time.html
 	}
 	const yScale = d3.scaleLinear() //linear range want quantitative information
 		.domain(d3.extent(dataset, domainLikes)) //find min and max of what's coming in from my dataseet
-		.range([screenHeight-margin, margin]); //what i want to come out on the screen
+		.range([1, screenHeight/2]); //what i want to come out on the screen
 	console.log(d3.extent(dataset, domainLikes))
+
+	const colScale = d3.scaleLinear() //linear range want quantitative information
+		// .domain([0,1]) //find min and max of what's coming in from my dataseet
+		.range(["red","blue"]); //what i want to come out on the screen
+
 /*step 6: set up fill scale according to color data
 read up here and see if you can figure something out:
 https://observablehq.com/@d3/color-schemes?collection=@d3/d3-scale-chromatic
@@ -68,12 +73,17 @@ check the week2/friday/bars.js example for more on this
 			return xScale(xAccessor(d)); //the x accessor parses the date for us
 		})
 		.attr("y", function(d){
-			return yScale(d.likes);
+			return screenHeight/2-yScale(parseInt(d.likes))/2;
 		})
-		.attr("width", 10)
+		.attr("width", 5)
 		.attr("height", function(d){
-			return screenHeight-yScale(d.likes);
+			console.log(yScale(parseInt(d.likes)))
+			return yScale(parseInt(d.likes));
 		})
-		.attr("fill","pink");
+		.attr("fill",function(d,i){
+			// colScale.range([d.color])
+			// console.log(colScale(i));
+			return d3.interpolateYlGn(.5);
+		});
 }
 drawData();
